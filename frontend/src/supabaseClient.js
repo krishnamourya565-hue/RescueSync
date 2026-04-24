a083a0b7a5e26eacda389d35d8fa1a4963f9fb40
RescueSync User <user@example.com>
2026-04-24 10:20:07 +0530

    Readme

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ercobawmabwsxgyjyiyr.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVyY29iYXdtYWJ3c3hneWp5aXlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NDYzNzIsImV4cCI6MjA5MjQyMjM3Mn0.0L2FgstEtzTJ-dJnPRjYEwP2BZE_2GM061XQnXMGepk';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
